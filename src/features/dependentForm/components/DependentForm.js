0fcb26fd71842296bc10365c6dc01a19cefbf5c0
Wilson Abesamis <wlabesamis@gmail.com>
2024-06-05 12:11:23 +0800

    removed unused hook and updated Readme

--- src/features/dependentForm/components/DependentForm.js
+++ src/features/dependentForm/components/DependentForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addInput, removeInput, updateInputData } from '../../dependentForm/dependentFormSlice';
 import Input from '../../../components/Input';
