1f79b483bf53062e80f2bb878681bfc64d3099da
Wilson Abesamis <wlabesamis@gmail.com>
2024-06-11 12:22:27 +0800

    added initial files for build process

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import store from './app/store';
 import DependentForm from './features/dependentForm/components/DependentForm';
 
 const App = () => {
+  console.log('API URL:', process.env.REACT_APP_API_URL);
+  console.log('Environment:', process.env.REACT_APP_ENV);
+  
   return (
     <Provider store={store}>
       <div>
